335d2911d084234e01507ef453a79e38ca16258b
Cris Mark <145134591+Tikboyong@users.noreply.github.com>
2026-01-13 20:02:45 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -14,40 +14,56 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const projectData = {
         1: {
-            title: "Project 1: My Project VAWC (Web & Mobile)",
+            title: "Project 1: My Project NeckShot Racing (Motorcycle Website)",
+            images: [
+                'project 5.5.png',
+                'project 5.6.png',
+                'project 5.7.png',
+                'project 5.8.png',
+            ]
+        },
+        2: {
+            title: "Project 2: Tasky Application (To-Do List Website)",
+            images: [
+                'project 6.1.png',
+                'project 6.2.png'
+            ]
+        },
+        3: {
+            title: "Project 3: Scientific-Calculator Applications",
+            images: [
+                'project 8.1.png',
+                'project 8.2.png',
+                'project 8.3.png',
+            ]
+        },
+        4: {
+            title: "Project 4: Sunshine Payroll Management System",
+            images: [
+                'project 3.1.png',
+                'project 3.3.png',
+                'project 3.4.png',
+                'project 3.5.png',
+            ]
+        },
+        5: {
+            title: "Project 5: E-Commerce Website ( Capstone Project )",
+            images: [
+                'project 4.1.png',
+                'project 4.2.png',
+                'project 4.3.png',
+                'project 4.4.png',
+            ]  
+        },
+        6: {
+            title: "Project 6: Project for the Barangay VAWC ( EXPIRED HOSTING )",
             images: [
                 'project 1.png',
                 'project 1.2.png',
                 'project 1.3.png',
-                'project 1.4.png'
-            ]
-        },
-        2: {
-            title: "Project 2: Facility Fables",
-            images: [
-                'project 2.png'
-            ]
-        },
-        3: {
-            title: "Project 3: SunShinePay",
-            images: [
-                'project 3.png',
-                'project 3.1.png',
-                'project 3.3.png',
-                'project 3.4.png',
-                'project 3.5.png'
-            ]
-        },
-        4: {
-            title: "Project 4: Data Visualization",
-            images: [
-                'project 4.png',
-                'project 4.1.png',
-                'project 4.2.png',
-                'project 4.3.png',
-                'project 4.4.png'
-            ]
-        }
+                'project 1.4.png',
+            ]  
+        }   
     };
 
     reviewButtons.forEach(button => {
